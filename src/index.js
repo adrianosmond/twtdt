a3fbc68cd5eb61d69b92783d7a25cb965a017a22
Adrian Osmond <a.c.osmond@gmail.com>
2021-01-14 07:44:21 +0100

    Remove default CRA ServiceWorker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { AppProvider } from 'contexts/AppContext';
 import App from './App';
-// import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <AppProvider>
@@ -10,8 +9,3 @@ ReactDOM.render(
   </AppProvider>,
   document.getElementById('root'),
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
