a60f6fe4dbacd5a8ecbea0a122fed4911f857810
Adrian Osmond <a.c.osmond@gmail.com>
2021-03-11 14:10:17 +0100

    Better mobile textarea

--- src/components/AppWrapper/AppWrapper.js
+++ src/components/AppWrapper/AppWrapper.js
@@ -1,5 +1,5 @@
 const AppWrapper = ({ children }) => (
-  <div className="flex flex-col m-0 mx-auto max-w-3xl py-6 px-4 min-h-screen sm:p-8 md:relative">
+  <div className="flex-grow flex flex-col m-0 mx-auto w-full max-w-3xl py-6 px-4 min-h-full sm:p-8 md:relative">
     {children}
   </div>
 );
