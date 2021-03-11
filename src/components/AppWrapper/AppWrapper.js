1ba3b975db199265972fca1a9281b1f22762c8ba
Adrian Osmond <a.c.osmond@gmail.com>
2021-03-11 14:23:00 +0100

    Use JS to fix mobile resizing

--- src/components/AppWrapper/AppWrapper.js
+++ src/components/AppWrapper/AppWrapper.js
@@ -1,7 +1,24 @@
-const AppWrapper = ({ children }) => (
-  <div className="flex-grow flex flex-col m-0 mx-auto w-full max-w-3xl py-6 px-4 min-h-full sm:p-8 md:relative">
-    {children}
-  </div>
-);
+import { useEffect, useState } from 'react';
+
+const AppWrapper = ({ children }) => {
+  const [height, setHeight] = useState(`${visualViewport.height}px`);
+
+  useEffect(() => {
+    const handler = (event) => {
+      setHeight(`${event.target.height}px`);
+    };
+    window.visualViewport.addEventListener('resize', handler);
+    return () => window.visualViewport.removeEventListener('resize', handler);
+  }, []);
+
+  return (
+    <div
+      className="flex-grow flex flex-col m-0 mx-auto w-full max-w-3xl py-6 px-4 sm:p-8 md:relative"
+      style={{ height }}
+    >
+      {children}
+    </div>
+  );
+};
 
 export default AppWrapper;
